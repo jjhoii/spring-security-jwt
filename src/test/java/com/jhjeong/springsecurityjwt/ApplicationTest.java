03383c92e9f88cff7d40dab3f076adb57edf89b5
jhjeong <kkiu1756@gmail.com>
2021-08-03 21:39:33 +0900

    프로젝트 생성

---
+++ src/test/java/com/jhjeong/springsecurityjwt/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jhjeong.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
