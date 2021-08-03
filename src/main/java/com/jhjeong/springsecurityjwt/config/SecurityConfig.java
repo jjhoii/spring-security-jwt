2f604ab7f84ad6e18338bdbc7ed3d88c88275d1d
jhjeong <kkiu1756@gmail.com>
2021-08-03 22:07:11 +0900

    시큐리티 설정

---
+++ src/main/java/com/jhjeong/springsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jhjeong.springsecurityjwt.config;
+
+import com.jhjeong.springsecurityjwt.model.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final CorsFilter corsFilter;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .addFilter(corsFilter) // @CrossOrigin: 인증이 없을 때, 인증이 있을 때는 필터에 등록
+        .formLogin().disable()
+        .httpBasic().disable()
+        .authorizeRequests()
+        .antMatchers("/api/v1/user/**")
+        .hasAnyRole(Role.USER.name(), Role.MANAGER.name(), Role.ADMIN.name())
+        .antMatchers("/api/v1/manager/**")
+        .hasAnyRole(Role.MANAGER.name(), Role.ADMIN.name())
+        .antMatchers("/api/v1/admin/**")
+        .hasRole(Role.ADMIN.name())
+        .anyRequest().permitAll();
+  }
+}
---
+++ src/main/java/com/jhjeong/springsecurityjwt/model/Role.java
@@ -0,0 +1,16 @@
+package com.jhjeong.springsecurityjwt.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+  USER("ROLE_USER", "사용자"),
+  MANAGER("ROLE_MANAGER", "매니저"),
+  ADMIN("ROLE_ADMIN", "관리자");
+
+  private final String key;
+  private final String title;
+}
