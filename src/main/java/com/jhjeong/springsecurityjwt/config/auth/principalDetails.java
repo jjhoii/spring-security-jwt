3b0e6c889c60ee91ca37155728b291fa4983efc9
jhjeong <kkiu1756@gmail.com>
2021-08-04 23:55:18 +0900

    principalDetails 생성

---
+++ src/main/java/com/jhjeong/springsecurityjwt/config/auth/principalDetails.java
@@ -0,0 +1,53 @@
+package com.jhjeong.springsecurityjwt.config.auth;
+
+import com.jhjeong.springsecurityjwt.model.User;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class principalDetails implements UserDetails {
+
+  private User user;
+
+  public principalDetails(User user) {
+    this.user = user;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    Collection<GrantedAuthority> authorities = new ArrayList<>();
+    user.getRoleList().forEach(r -> authorities.add(() -> r));
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
